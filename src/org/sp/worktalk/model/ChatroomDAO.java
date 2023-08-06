ab5c834d458f815192807606a77acf797a99d98c
jiin27 <132329475+jiin27@users.noreply.github.com>
2023-08-06 20:31:54 +0900

    dao

--- src/org/sp/worktalk/model/ChatroomDAO.java
+++ src/org/sp/worktalk/model/ChatroomDAO.java
@@ -3,9 +3,11 @@ package org.sp.worktalk.model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import org.sp.worktalk.util.DBManager;
 
+//채팅방 목록. 채팅방 별 고유 번호, 채팅방 이름, 라스트 메시지 보유
 public class ChatroomDAO {
 	DBManager dbManager;
 	
@@ -13,12 +15,26 @@ public class ChatroomDAO {
 		this.dbManager = dbManager;
 	}
 	
-	public void selectChatroomInfo() {
+	//채팅이 시작되면, 생성된 채팅방 idx생성, 채팅방 이름은 접속자의 '사원이름'넣기, 마지막 메시지 들어가기(lastindex?)
+	public void insert() {
 		Connection con=null;
 		PreparedStatement pstmt=null;
-		ResultSet rs=null;
+		int result=0;
 		
-		dbManager.connect();
+		con=dbManager.connect();
+		
+		StringBuilder sb=new StringBuilder();
+		sb.append("insert into chatroom(chatroom_idx, chatroom_name, lastmessage)");
+		sb.append(" values(seq_chatroom.nextval, ?, ?)");
+		
+		try {
+			pstmt=con.prepareStatement(sb.toString());
+			pstmt.setInt(1, ); //접속자 이름
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		String sql="select * from chatroom";
 	}
--- src/org/sp/worktalk/model/MessageDAO.java
+++ src/org/sp/worktalk/model/MessageDAO.java
@@ -1,5 +1,45 @@
 package org.sp.worktalk.model;
 
-public class MessageDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import org.sp.worktalk.domain.Message;
+import org.sp.worktalk.util.DBManager;
+
+public class MessageDAO {
+	DBManager dbManager;
+	
+	public MessageDAO(DBManager dbManager) {
+		this.dbManager=dbManager;
+		
+	}
+	
+	//서버로부터 수신받은 메시지를 저장, 
+	public int insert(Message message) {
+		Connection	con=null;
+		PreparedStatement pstmt=null;
+		int result=0;
+		
+		con=dbManager.connect();
+		
+		StringBuilder sb=new StringBuilder();
+		sb.append("insert into Message(message_idx, room_mate_idx, msg)");
+		sb.append(" values(seq_message.nextval, ?, ?)");
+		
+		//바인드변수 정의
+		try {
+			pstmt=con.prepareStatement(sb.toString());
+			pstmt.setInt(1, message.getRoom_mate_idx());
+			pstmt.setString(2, message.getMsg());
+			
+			result=pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			dbManager.release(con, pstmt);
+		}
+		return result;
+	}
 }
